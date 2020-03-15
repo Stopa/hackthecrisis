32c3aeb77c731413a96af0393883e20c6ca551a9
Stepan Bolotnikov <stepan.bolotnikov@proekspert.ee>
2020-03-15 12:17:13 +0200

    Basic layout, main stream and webcam

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,20 @@
-import VJS from 'video.js';
+import videojs from 'video.js';
+require('@videojs/http-streaming');
 
-console.log(VJS);
+function getCamera() {
+  const video = document.getElementById('webcam-video');
+
+  navigator.mediaDevices.getUserMedia({
+    video: true
+  }).then(stream => video.srcObject = stream);
+}
+
+function initMainStream() {
+  const mainStream = videojs('main-stream', { autoplay: true });
+  mainStream.volume(0.1);
+}
+
+window.onload = () => {
+  getCamera()
+  initMainStream();
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -15,5 +16,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/index.html'),
     }),
+    new CopyPlugin([{
+      from: path.resolve(__dirname, 'src/css'),
+      to: path.resolve(__dirname, 'dist'),
+    }]),
   ],
 };
